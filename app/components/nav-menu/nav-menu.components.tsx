4a569030ea021f2608dd53451a630e6361267c24
carterbacon <student@learncodinganywhere.com>
2023-11-09 17:31:32 -0800

    created NavMenu component and stylesheet

---
+++ app/components/nav-menu/nav-menu.components.tsx
@@ -0,0 +1,7 @@
+const NavMenu = () => {
+  return (
+    <div>NavMenu</div>
+  )
+};
+
+export default NavMenu;
--- app/components/nav/nav.component.tsx
+++ app/components/nav/nav.component.tsx
@@ -1,10 +1,12 @@
 // components
 import Logo from "../logo/logo.component";
+import NavMenu from "../nav-menu/nav-menu.components";
 
 const Nav = () => {
   return (
     <header className="nav">
       <Logo />
+      <NavMenu />
     </header>
   )
 };
