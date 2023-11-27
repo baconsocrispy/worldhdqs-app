8fd8a53dbe917ec3c3a4cd225f372471387a14f3
carterbacon <student@learncodinganywhere.com>
2023-11-27 15:45:50 -0800

    cleaned hero component

--- app/components/hero/hero.component.tsx
+++ app/components/hero/hero.component.tsx
@@ -1,12 +1,11 @@
-// components
-import Image from "next/image";
-
 const Hero = () => {
   return (
     <section className="hero">
       <div className="hero__content">
         <h1 className="hero__header">Welcome to World Headquarters</h1>
-        <p className="hero__text">Custom Software Solutions & Creative Workshop</p>
+        <p className="hero__text">
+          Custom Software Solutions & Creative Workshop
+        </p>
       </div>
     </section>
   )
