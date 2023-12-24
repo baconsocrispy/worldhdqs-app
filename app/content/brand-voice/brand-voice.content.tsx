0fc9f9429804afa07b1792bc63b7a4b09861c2eb
carterbacon <student@learncodinganywhere.com>
2023-12-24 14:41:12 -0800

    troubleshooting brand voice mobile text color

--- app/content/brand-voice/brand-voice.content.tsx
+++ app/content/brand-voice/brand-voice.content.tsx
@@ -28,7 +28,7 @@ const BrandVoice: FC<BrandVoiceProps> = ({ stringList = [] }) => {
                 transitionFunction: 'ease-in'
               }}
               index={ index }
-              intersectionOptions={{ threshold: 0.9 }}
+              intersectionOptions={{ root: null, threshold: 0.1 }}
               className="randomizer__content"
               key={ index }
               style={{
