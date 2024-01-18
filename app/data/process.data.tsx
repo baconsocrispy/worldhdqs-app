2fadaf4da812f158122dd507b74a4377a3bd8adb
carterbacon <student@learncodinganywhere.com>
2024-01-18 12:50:09 -0800

    fixed typos in process section

--- app/data/process.data.tsx
+++ app/data/process.data.tsx
@@ -4,7 +4,7 @@ export const processItems: ListItem[] = [
   {
     image: { id: 0, src: './graphics/SVG/mittens_1.svg' },
     textHighlight: 'Meet & Greet',
-    text: 'Whatever your problem is, we want to hear about it and see if we can\'t help you find a creative solution.'
+    text: 'Whatever your problem is, we want to hear about it and see if we can help you find a creative solution.'
   },
   {
     image: { id: 1, src: './graphics/SVG/blueprint_2.svg' },
@@ -19,11 +19,11 @@ export const processItems: ListItem[] = [
   {
     image: { id: 3, src: './graphics/SVG/testing_1.svg' },
     textHighlight: 'Testing & Revision',
-    text: 'When we have a version of the project that satisfies all of your requirements, we will put it through it\'s paces and send it back to you for comments and revisions.'
+    text: 'When we have a version of the project that satisfies all of your requirements, we will put it through its paces and send it back to you for comments and revisions.'
   },
   {
     image: { id: 4, src: './graphics/SVG/production.svg' },
     textHighlight: 'Final Delivery & Production',
     text: 'After polishing the project based on the revision round  and sealing it with your approval, we will deploy it to a production environment that is sensitive to ongoing costs and future maintenance.'
-  },
+  }
 ];
